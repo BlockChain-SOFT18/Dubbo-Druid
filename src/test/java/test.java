e9c5a9f0a7adc97fcc308b11258973fba8c3f8fb
jj19971130 <1901014432@qq.com>
2018-04-24 13:06:00 +0800

    1

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,13 +1,25 @@
 import jj.druid.provider.Druid;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class test {
-    public static void main(String[] args) throws SQLException {
-        Druid druid = new Druid();
-        druid.getDruidDataSource();
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"Druid.xml","Mybatis.xml"});
+        context.start();
+        final SqlSessionFactory factory = (SqlSessionFactory) context.getBean("sqlSessionFactory");
+        SqlSession session = factory.openSession();
+        List l = session.selectList("selectAll");
+        System.out.println(l);
+        Thread thread = new Thread() {
+            public void run() {
+
+            }
+        };
     }
 }
